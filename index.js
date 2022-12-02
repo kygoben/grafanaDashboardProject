1ad47b549ee46469615bb0ad1a8a4128c5800dea
Kyle Goben <kygoben@gmail.com>
2022-12-02 00:30:22 -0600

    traffic counter and response time counter

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ import { collectDefaultMetrics, register } from 'prom-client';
 collectDefaultMetrics();
 
 const app = express();
+
+const gauge = new Gauge({ name: 'api_http_requests_total', help: "api_http_requests_total_help", labelNames: ['custom'] });
+register.registerMetric(gauge);
+register.registerMetric(httpRequestDurationMicroseconds);
+collectDefaultMetrics({ register });
+gauge.set(0);
+
+app.use((req, res, next) => {
+    res.locals.startEpoch = Date.now()
+    next()
+})
+
+app.use((req, res, next) => { gauge.inc(1); next(); });
+
 //The grafana agents calls this metrics route to get metrics from the node app
 app.get('/metrics', async (_req, res) => {
   try {
@@ -14,5 +28,15 @@ app.get('/metrics', async (_req, res) => {
     res.status(500).end(err);
   }
 });
+
+
+app.use((req, res, next) => {
+  const responseTimeInMs = new Date() - res.locals.startEpoch;
+  httpRequestDurationMicroseconds
+      .labels(req.route.path)
+      .observe(responseTimeInMs);
+
+  next();
+})
 
 app.listen(4001, '0.0.0.0');
